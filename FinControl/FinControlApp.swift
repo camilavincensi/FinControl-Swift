9b377ac0e8fe5dc48f6b4284a0e003dbd1329193
camila-dv@hotmail.com <camila.vincensi@cresol.com.br>
2026-01-04 15:52:29 -0300

    Initial Commit

---
+++ FinControl/FinControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinControlApp.swift
+//  FinControl
+//
+//  Created by Camila Vincensi on 04/01/26.
+//
+
+import SwiftUI
+
+@main
+struct FinControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
